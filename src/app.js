86e0f7ae0bab4a007cd0b55542dd2accc6707eb3
Solm1504 <solm1504@gmail.com>
2023-05-07 17:27:23 +0000

    main

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const generarNombres = () => {
+    const pronoun = ["the", "our"];
+    const adj = ["great", "big"];
+    const noun = ["jogger", "racoon"];
+    const domains = [];
+
+    pronoun.forEach(pronounItem => {
+      adj.forEach(adjItem => {
+        noun.forEach(nounItem => {
+          const domainName = `${pronounItem}${adjItem}${nounItem}.com`;
+          domains.push(domainName);
+        });
+      });
+    });
+
+    console.log(domains);
+  };
 };
